12806d7cbfd2ea94d945353e8dca111cc358bd30
Diablo <hpro3961@gmail.com>
2025-04-17 01:01:23 -0600

    cambios a pestaña_carrera y a carrusel

--- resources/views/components/pestanas_carrera.blade.php
+++ resources/views/components/pestanas_carrera.blade.php
@@ -3,23 +3,25 @@
     <div class="tab" onclick="showTab('Tecnologia')">Programación y Tecnologías</div>
     <div class="tab" onclick="showTab('Industria')">Industria y Mantenimiento</div>
     <div class="tab" onclick="showTab('hoteleria')">Hotelería y Servicios</div>
+    <div class="indicator" id="indicator"></div>
 </div>
+
 
 <div id="admin-gestion" class="content active">
     <div class="program">
         <img src="{{ asset('imagenes/Proceso_Gestion_Administrativa.jpg') }}" alt="Procesos de Gestión Administrativa" class="program-img">
         <h3>Procesos de Gestión Administrativa</h3>
-        <button onclick="showDetails('gestion-administrativa')">Ver más</button>
-    </div>
+        <button onclick="location.href='{{ route('Carreras') }}'">Ver más</button>
+        </div>
     <div class="program">
         <img src="{{ asset('imagenes/Produccion_Industrial.jpg') }}" alt="Producción Industrial" class="program-img">
         <h3>Producción Industrial</h3>
-        <button onclick="showDetails('produccion-industrial')">Ver más</button>
-    </div>
+        <button onclick="location.href='{{ route('Carreras') }}'">Ver más</button>
+        </div>
     <div class="program">
         <img src="{{ asset('imagenes/Produccion_Industrial_de_Alimentos.jpg') }}" alt="Producción Industrial de Alimentos" class="program-img">
         <h3>Producción Industrial de Alimentos</h3>
-        <button onclick="showDetails('produccion-industrial')">Ver más</button>
+        <button onclick="location.href='{{ route('Carreras') }}'">Ver más</button>
     </div>
 </div>
 
@@ -27,17 +29,17 @@
     <div class="program">
         <img src="{{ asset('imagenes/Programacion.jpg') }}" alt="Programación" class="program-img">
         <h3>Programación</h3>
-        <button onclick="showDetails('programacion')">Ver más</button>
+        <button onclick="location.href='{{ route('Carreras') }}'">Ver más</button>
     </div>
     <div class="program">
-        <img src="{{ asset('imagenes/gestion-produccion-industrial.jpg') }}" alt="Mecatrónica" class="program-img">
-        <h3>Mecatrónica</h3>
-        <button onclick="showDetails('programacion')">Ver más</button>
+        <img src="{{ asset('imagenes/Electronica.jpg') }}" alt="Electronica" class="program-img">
+        <h3>Electronica</h3>
+        <button onclick="location.href='{{ route('Electronica') }}'">Ver más</button>
     </div>
     <div class="program">
-        <img src="{{ asset('imagenes/gestion-produccion-industrial.jpg') }}" alt="Técnico en Mecatrónica" class="program-img">
+        <img src="{{ asset('imagenes/Mecatronica.jpg') }}" alt="Técnico en Mecatrónica" class="program-img">
         <h3>Técnico en Mecatrónica</h3>
-        <button onclick="showDetails('programacion')">Ver más</button>
+        <button onclick="location.href='{{ route('Carreras') }}'">Ver más</button>
     </div>
 </div>
 
@@ -55,7 +57,7 @@
     <div class="program">
         <img src="{{ asset('imagenes/gestion-produccion-industrial.jpg') }}" alt="Transformación de Plásticos" class="program-img">
         <h3>Transformación de Plásticos</h3>
-        <button onclick="showDetails('programacion')">Ver más</button>
+        <button onclick="location.href='{{ route('Carreras') }}'">Ver más</button>
     </div>
 </div>
 
@@ -63,7 +65,7 @@
     <div class="program">
         <img src="{{ asset('imagenes/Servicio_hoteleria.jpg') }}" alt="Hotelería y Servicios" class="program-img">
         <h3>Servicios de Hotelería</h3>
-        <button onclick="showDetails('hoteleria-servicios')">Ver más</button>
+        <button onclick="location.href='{{ route('Carreras') }}'">Ver más</button>
     </div>
 </div>
 
