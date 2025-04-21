d96cc608f6ae802b98c5254b83702f412e242ce7
Diablo <hpro3961@gmail.com>
2025-04-21 11:36:08 -0600

    cambios 210/04/25

---
+++ resources/views/Transparencia.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Convenios')
+<link href="{{ asset(path: 'css/styles_convenios.css') }}" rel="stylesheet">
+
+@section('body-class', 'no-background')
+
+@section('title', 'Transparencia')
+
+@section('content')
+
+@endsection
+
+@push('scripts')
+@endpush
