5d116e8f67dcc0fd0a05b25f2fe53673b88a2b45
Diablo <hpro3961@gmail.com>
2025-04-25 09:37:24 -0600

    se enruta bolsa de trabajo y se mete bolsa-convenios en vinculacion

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,9 +6,8 @@
         </a>
 
         <!-- Hamburguesa -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNav" aria-controls="navbarNav"
-                aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -24,34 +23,47 @@
                 {{-- Dropdown --}}
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="ofertaDropdown" role="button"
-                       data-bs-toggle="dropdown" aria-expanded="false">
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         Oferta Educativa
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="ofertaDropdown">
                         <li><a class="dropdown-item" href="{{ route('Carreras') }}">Programación</a></li>
-                        <li><a class="dropdown-item" href="{{ route('Fuentes_Alternas') }}">Fuentes Alternas de Energía</a></li>
-                        <li><a class="dropdown-item" href="{{ route('Mantenemiento_Automotriz') }}">Mantenimiento Automotriz</a></li>
-                        <li><a class="dropdown-item" href="{{ route('Mantenimento_Industrial') }}">Mantenimiento Industrial</a></li>
+                        <li><a class="dropdown-item" href="{{ route('Fuentes_Alternas') }}">Fuentes Alternas de
+                                Energía</a></li>
+                        <li><a class="dropdown-item" href="{{ route('Mantenemiento_Automotriz') }}">Mantenimiento
+                                Automotriz</a></li>
+                        <li><a class="dropdown-item" href="{{ route('Mantenimento_Industrial') }}">Mantenimiento
+                                Industrial</a></li>
                         <li><a class="dropdown-item" href="{{ route('Electronica') }}">Electrónica</a></li>
-                        <li><a class="dropdown-item" href="{{ route('Mantenimientomotoresdecombustion') }}">Motores de Combustión</a></li>
-                        <li><a class="dropdown-item" href="{{ route('Proceso-de-Gestión') }}">Proceso de Gestión</a></li>
+                        <li><a class="dropdown-item" href="{{ route('Mantenimientomotoresdecombustion') }}">Motores de
+                                Combustión</a></li>
+                        <li><a class="dropdown-item" href="{{ route('Proceso-de-Gestión') }}">Proceso de Gestión</a>
+                        </li>
                         <li><a class="dropdown-item" href="{{ route('Hoteleria') }}">Hotelería</a></li>
                         <li><a class="dropdown-item" href="#">Semiconductores</a></li>
                         <li><a class="dropdown-item" href="#">Inteligencia Artificial</a></li>
                         <li><a class="dropdown-item" href="#">Electromovilidad</a></li>
                     </ul>
                 </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="ofertaDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Vinculación
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="ofertaDropdown">
+                        <li><a class="dropdown-item" href="{{ route('convenios') }}">Convenios</a></li>
+                        <li><a class="dropdown-item" href="{{ route('bolsa') }}">Bolsa de Trabajo</a></li>
+                    </ul>
+                </li>
 
                 <li class="nav-item"><a class="nav-link" href="{{ route('Planteles') }}">Planteles</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Vinculación</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('convenios') }}">Convenios</a></li>
             </ul>
 
             {{-- Búsqueda --}}
             <div class="d-flex ms-3">
-                <button class="btn btn-outline-light search-button" type="button"
-                        data-bs-toggle="collapse" data-bs-target="#search-bar"
-                        aria-expanded="false" aria-controls="search-bar" title="Buscar">
+                <button class="btn btn-outline-light search-button" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#search-bar" aria-expanded="false" aria-controls="search-bar" title="Buscar">
                     <img src="{{ asset('imagenes/lupa-icon.png') }}" alt="Buscar" width="28" height="28">
                 </button>
                 <div class="collapse" id="search-bar">
