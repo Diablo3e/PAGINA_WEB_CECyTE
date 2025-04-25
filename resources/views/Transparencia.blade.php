fc9eb47d4c41986448e1c864b8ea4d43f4c92748
Diablo <hpro3961@gmail.com>
2025-04-25 10:56:41 -0600

    añadi informacion

--- resources/views/Transparencia.blade.php
+++ resources/views/Transparencia.blade.php
@@ -18,133 +18,190 @@
 
     {{-- Tarjetas superiores --}}
     <div class="row justify-content-center g-4 mb-4">
-      <div class="col-12 col-md-4">
-        <a href="#" class="d-block text-center p-3 cta-card">
-          <img src="https://cecytepuebla.edu.mx/wp-content/uploads/elementor/thumbs/logo-pnt-pjckdpo5vb0iakjov9esqkwc3l3u58mj0gv9ks9t90.png" class="img-fluid" alt="Plataforma Nacional de Transparencia">
-        </a>
-      </div>
-      <div class="col-12 col-md-4">
-        <a href="#" class="d-block text-center p-3 cta-card">
-          <img src="https://cecytepuebla.edu.mx/wp-content/uploads/elementor/thumbs/logo-obligaciones-pjckdkyyx4u2oiqimpdnw4314nr02r3vbtlu6egs44.png" class="img-fluid" alt="Obligaciones de Transparencia">
-        </a>
-      </div>
+    <div class="col-12 col-md-4">
+      <a href="#" class="d-block text-center p-3 cta-card">
+      <img
+        src="https://cecytepuebla.edu.mx/wp-content/uploads/elementor/thumbs/logo-pnt-pjckdpo5vb0iakjov9esqkwc3l3u58mj0gv9ks9t90.png"
+        class="img-fluid" alt="Plataforma Nacional de Transparencia">
+      </a>
+    </div>
+    <div class="col-12 col-md-4">
+      <a href="#" class="d-block text-center p-3 cta-card">
+      <img
+        src="https://cecytepuebla.edu.mx/wp-content/uploads/elementor/thumbs/logo-obligaciones-pjckdkyyx4u2oiqimpdnw4314nr02r3vbtlu6egs44.png"
+        class="img-fluid" alt="Obligaciones de Transparencia">
+      </a>
+    </div>
     </div>
 
     <h2 class="fw-bold mb-4">
-      En cumplimiento al título V de la Ley General de Contabilidad Gubernamental y la Ley de Transparencia
+    En cumplimiento al título V de la Ley General de Contabilidad Gubernamental y la Ley de Transparencia
     </h2>
 
     {{-- Acordeón principal --}}
     <div class="accordion" id="acordeonTransparencia">
 
-      {{-- ITEM 1 – LGCG --}}
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="h1">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#c1" aria-expanded="true" aria-controls="c1">
-            Información Presupuestal
-          </button>
-        </h2>
-        <div id="c1" class="accordion-collapse collapse show" aria-labelledby="h1">
-          <div class="accordion-body p-0">
-            <ul class="list-group list-group-flush">
+    {{-- ITEM 1 – LGCG --}}
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="h1">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#c1"
+        aria-expanded="true" aria-controls="c1">
+        Información Presupuestal
+      </button>
+      </h2>
+      <div id="c1" class="accordion-collapse collapse show" aria-labelledby="h1">
+      <div class="accordion-body p-0">
+        <ul class="list-group list-group-flush">
 
-{{-- Submenú: Cuentas públicas --}}
-<li class="list-group-item p-0">
-  <button class="accordion-submenu submenu collapsed py-2 px-3" type="button" data-bs-toggle="collapse"
+        {{-- Submenú: Cuentas públicas --}}
+        <li class="list-group-item p-0">
+          <button class="accordion-submenu submenu collapsed py-2 px-3" type="button" data-bs-toggle="collapse"
           data-bs-target="#cuentasAnios" aria-expanded="false" aria-controls="cuentasAnios">
-    CUENTAS PÚBLICAS
-  </button>
-  <div id="cuentasAnios" class="collapse">
-    <ul class="inner-menu list-unstyled">
-      <li><a href="#">2019</a></li>
-      <li><a href="#">2020</a></li>
-      <li><a href="#">2021</a></li>
-      <li><a href="#">2022</a></li>
-    </ul>
-  </div>
-</li>
-
-
-              {{-- Submenú: Ayudas y Subsidios --}}
-              <li class="list-group-item p-0">
-                <button class="accordion-button submenu collapsed py-2 px-3" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#ayudasAnios" aria-expanded="false" aria-controls="ayudasAnios">
-                  AYUDAS Y SUBSIDIOS
-                </button>
-                <div id="ayudasAnios" class="collapse">
-                  <ul class="inner-menu list-unstyled">
-                    <li><a href="#">2023</a></li>
-                    <li><a href="#">2022</a></li>
-                    <li><a href="#">2021</a></li>
-                  </ul>
-                </div>
-              </li>
-
-              {{-- Enlace simple --}}
-              <li class="list-group-item">
-                <a href="#">PROGRAMAS PRESUPUESTARIOS</a>
-              </li>
-
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      {{-- ITEM 2 – LDF --}}
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="h2">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#c2" aria-expanded="false" aria-controls="c2">
-            Indicadores de Desempeño
+          CUENTAS PÚBLICAS
           </button>
-        </h2>
-        <div id="c2" class="accordion-collapse collapse" aria-labelledby="h2">
-          <div class="accordion-body">
-            <a href="#">Ver documentos</a>
+          <div id="cuentasAnios" class="collapse">
+          <ul class="inner-menu list-unstyled">
+            <li><a href="#">2019</a></li>
+            <li><a href="#">2020</a></li>
+            <li><a href="#">2021</a></li>
+            <li><a href="#">2022</a></li>
+          </ul>
           </div>
+        </li>
+
+
+        {{-- Submenú: Ayudas y Subsidios --}}
+        <li class="list-group-item p-0">
+          <button class="accordion-button submenu collapsed py-2 px-3" type="button" data-bs-toggle="collapse"
+          data-bs-target="#ayudasAnios" aria-expanded="false" aria-controls="ayudasAnios">
+          AYUDAS Y SUBSIDIOS
+          </button>
+          <div id="ayudasAnios" class="collapse">
+          <ul class="inner-menu list-unstyled">
+            <li><a href="#">2017</a></li>
+            <li><a href="#">2018</a></li>
+            <li><a href="#">2019</a></li>
+            <li><a href="#">2020</a></li>
+            <li><a href="#">2021</a></li>
+            <li><a href="#">2022</a></li>
+            <li><a href="#">2023</a></li>
+            <li><a href="#">2024</a></li>
+          </ul>
+          </div>
+        </li>
+
+
+
+        </ul>
+        {{-- Enlace simple --}}
+        <li class="list-group-item">
+        <button class="accordion-button submenu collapsed py-2 px-3" type="button" data-bs-toggle="collapse"
+          data-bs-target="#programaAnios" aria-expanded="false" aria-controls="programaAnios">
+          Programas Presupuestarios
+        </button>
+        <div id="programaAnios" class="collapse">
+          <ul class="inner-menu list-unstyled">
+
+          <li><a href="#">2020</a></li>
+
+          </ul>
         </div>
+        </li>
+      </div>
+      </div>
+    </div>
+
+    {{-- ITEM 2 – LDF --}}
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="h2">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#c2"
+        aria-expanded="false" aria-controls="c2">
+        Indicadores de Desempeño
+      </button>
+      </h2>
+      <div id="c2" class="accordion-collapse collapse" aria-labelledby="h2">
+      <div class="accordion-body">
+        <ul class="inner-menu list-unstyled">
+        <li><a href="#">2013</a></li>
+        <li><a href="#">2014</a></li>
+        <li><a href="#">2015</a></li>
+        <li><a href="#">2016</a></li>
+        <li><a href="#">2017</a></li>
+        <li><a href="#">2018</a></li>
+        <li><a href="#">2019</a></li>
+        <li><a href="#">2020</a></li>
+        <li><a href="#">2021</a></li>
+        <li><a href="#">2022</a></li>
+        <li><a href="#">2023</a></li>
+        <li><a href="#">2024</a></li>
+        </ul>
+
+      </div>
+      </div>
+    </div>
+    {{-- ITEM 4 – Declaraciones Patrimoniales --}}
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="h4">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#c4"
+        aria-expanded="false" aria-controls="c4">
+        Información Financiera Trimestral
+      </button>
+      </h2>
+      <div id="c4" class="accordion-collapse collapse" aria-labelledby="h4">
+      <div class="accordion-body">
+        <ul class="inner-menu list-unstyled">
+        <a href="#">Manual de Contabilidad Gubernamental  </a>
+        <a href="#">Indicadorex de Postura Fiscal</a>
+        <a href="#">Indicadores de Resultados</a>
+        <a href="#">Programas de Proyectos de Inversión</a>
+
+        </ul>
+
+        <ul class="inner-menu list-unstyled">
+        <li><a href="#">2013</a></li>
+        <li><a href="#">2014</a></li>
+        <li><a href="#">2015</a></li>
+        <li><a href="#">2016</a></li>
+        <li><a href="#">2017</a></li>
+        <li><a href="#">2018</a></li>
+        <li><a href="#">2019</a></li>
+        <li><a href="#">2020</a></li>
+        <li><a href="#">2021</a></li>
+        <li><a href="#">2022</a></li>
+        <li><a href="#">2023</a></li>
+        <li><a href="#">2024</a></li>
+        </ul>
+
+      </div>
+      </div>
+    </div>
+
+    {{-- ITEM 5 – PMD 2024 --}}
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="h5">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#c5"
+        aria-expanded="false" aria-controls="c5">
+        Inventario </button>
+      </h2>
+      <div id="c5" class="accordion-collapse collapse" aria-labelledby="h5">
+      <div class="accordion-body">
+        <ul class="inner-menu list-unstyled">
+        <li><a href="#">2014</a></li>
+        <li><a href="#">2015</a></li>
+        <li><a href="#">2016</a></li>
+        <li><a href="#">2017</a></li>
+        <li><a href="#">2018</a></li>
+        <li><a href="#">2019</a></li>
+        <li><a href="#">2020</a></li>
+        <li><a href="#">2021</a></li>
+        <li><a href="#">2022</a></li>
+        <li><a href="#">2023</a></li>
+        <li><a href="#">2024</a></li>
+        </ul>
       </div>
 
-      {{-- ITEM 3 – Actas Comité --}}
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="h3">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#c3" aria-expanded="false" aria-controls="c3">
-            ACTAS DEL COMITÉ DE TRANSPARENCIA
-          </button>
-        </h2>
-        <div id="c3" class="accordion-collapse collapse" aria-labelledby="h3">
-          <div class="accordion-body">
-            <a href="#">Ver actas</a>
-          </div>
-        </div>
       </div>
-
-      {{-- ITEM 4 – Declaraciones Patrimoniales --}}
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="h4">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#c4" aria-expanded="false" aria-controls="c4">
-            Información Financiera Trimestral
-          </button>
-        </h2>
-        <div id="c4" class="accordion-collapse collapse" aria-labelledby="h4">
-          <div class="accordion-body">
-            <a href="#">Ver declaraciones</a>
-          </div>
-        </div>
-      </div>
-
-      {{-- ITEM 5 – PMD 2024 --}}
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="h5">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#c5" aria-expanded="false" aria-controls="c5">
-            EVALUACIÓN DEL PMD 2024
-          </button>
-        </h2>
-        <div id="c5" class="accordion-collapse collapse" aria-labelledby="h5">
-          <div class="accordion-body">
-            <a href="#">Ver evaluación</a>
-          </div>
-        </div>
-      </div>
+    </div>
 
     </div><!-- /accordion principal -->
 
