7550c0e09dfdfd496dbcce8be536a87c296991ed
Ernesto ML <ernesto.montano@upaep.mx>
2025-06-24 15:26:13 -0600

    Carrusel main page a bootstrap
    Se cambio el carrusel de la pagina principal para que use los elementos de bootstrap en vez de los personalizados para mejorar responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 @routes
 @section('title', 'CECyTE')
-<link href="{{ asset('css/styles_mapa_carrusel.css') }}" rel="stylesheet">
 <link href="{{ asset('css/styles_pestanas_carrera.css') }}" rel="stylesheet">
 <link href="{{ asset('css/styles_planteles.css') }}" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
@@ -25,7 +24,6 @@
 @push('scripts')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="{{ asset('js/pestanas_carrera.js') }}"></script>
-<script src="{{ asset('js/carrusel.js') }}"></script>
 <script src="{{ asset('js/main_formulario.js') }}"></script>
 @endpush
 
