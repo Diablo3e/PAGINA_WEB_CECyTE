b7bc0ba04bd69726a43d878e0bb911622963ec0d
Ernesto ML <ernesto.montano@upaep.mx>
2025-07-07 22:17:41 -0600

    Correccion "temporal" formulario
    Se intento arreglar un bug en el que muchos cambios en la eleccion de plantel causan que el apartado de las carreras no muestren "Bachillerato general" en instituciones EMSAD

--- public/js/main_formulario.js
+++ public/js/main_formulario.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('#planteles').on('change', function() {
+$(document).ready(function () {
+    $('#planteles').on('change', function () {
         var plantelId = $(this).val();
         var $carreras = $('#carreras');
         $carreras.html('<option value="">Cargando...</option>');
@@ -9,21 +9,28 @@ $(document).ready(function() {
             return;
         }
 
-        $.ajax({
+        let peticionAJAX = null;
+        if(peticionAJAX){
+            peticionAJAX.abort();
+        }
+
+        peticionAJAX = $.ajax({
             url: route('carreras.por.plantel', plantelId),
             type: 'GET',
             dataType: 'json',
             success: function(data) {
-                if($.isEmptyObject(data)){
-                    $carreras.html('<option value="">Bachillerato general</option>');
-                    $carreras.prop('disabled', true);
-                }else{
+                //fixme: el timeout no es ideal, pero cubre la mayoria de los bugs y ya intente todo lo que se me ocurrio sin mucho mas exito 
+                setTimeout(null, 500);
+                if(!$.isEmptyObject(data)){
                     var options = '<option value="">Elige tu carrera</option>';
                     $.each(data, function(index, carrera) {
                         options += '<option value="' + carrera.id + '">' + carrera.nombre + '</option>';
                     });
                     $carreras.prop('disabled', false);
                     $carreras.html(options);
+                }else{
+                    $carreras.html('<option value="">Bachillerato general</option>');
+                    $carreras.prop('disabled', true);
                 }
             },
             error: function() {
