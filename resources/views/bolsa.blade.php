d09567d0ac9eba050518ef92090043de96fc12f5
Ernesto ML <ernesto.montano@upaep.mx>
2025-07-25 21:04:33 -0600

    Abrir links en bolsa en nueva pestaña

--- resources/views/bolsa.blade.php
+++ resources/views/bolsa.blade.php
@@ -22,10 +22,10 @@
         
         <div class="divEmpleos py-3">
             <ol>
-                <li>Empresa en 👉 <a href="https://www.grupogateway.mx">www.grupogateway.mx</a></li>
-                <li><a href="https://www.occ.com.mx/candidatos/cuenta-nueva/registro/?utm_source=google&utm_medium=cpc&utm_campaign=21095626929&keyword=occ&gad_source=1&gad_campaignid=21095626929&gbraid=0AAAAADQ3s09cxB1vM_xavwkwKiKaP-D2m&gclid=Cj0KCQjws4fEBhD-ARIsACC3d29Oj6hvKoPVEr4GaE-A3X_ZgdRcuh6wOpi22XBckZl8MiCE46YsqR4aAuA-EALw_wcB">OCC empleos</a></li>
-                <li><a href="https://www.empleo.gob.mx/PortalDigitali">SNE SERVICIO NACIONAL DE EMPLEO</a></li>
-                <li><a href="https://mx.indeed.com/q-reci%C3%A9n-egresados-para-l-puebla,-pue.-empleos.html?aceid=&gad_source=1&gad_campaignid=18740264862&gbraid=0AAAAADes5imafDRymhnUBKuD3atXWIns9&gclid=Cj0KCQjws4fEBhD-ARIsACC3d29RPpP_iW5Hd3vhzPgGpjMQVnpXV8aORMdJg0VimjkpqAeafoWj9jAaAqcTEALw_wcB&gclsrc=aw.ds&vjk=ca9fc981455db544">INDEED</a></li>
+                <li>Empresa en 👉 <a href="https://www.grupogateway.mx" target="_blank">www.grupogateway.mx</a></li>
+                <li><a href="https://www.occ.com.mx/candidatos/cuenta-nueva/registro/?utm_source=google&utm_medium=cpc&utm_campaign=21095626929&keyword=occ&gad_source=1&gad_campaignid=21095626929&gbraid=0AAAAADQ3s09cxB1vM_xavwkwKiKaP-D2m&gclid=Cj0KCQjws4fEBhD-ARIsACC3d29Oj6hvKoPVEr4GaE-A3X_ZgdRcuh6wOpi22XBckZl8MiCE46YsqR4aAuA-EALw_wcB" target="_blank">OCC empleos</a></li>
+                <li><a href="https://www.empleo.gob.mx/PortalDigital" target="_blank">SNE SERVICIO NACIONAL DE EMPLEO</a></li>
+                <li><a href="https://mx.indeed.com/q-reci%C3%A9n-egresados-para-l-puebla,-pue.-empleos.html?aceid=&gad_source=1&gad_campaignid=18740264862&gbraid=0AAAAADes5imafDRymhnUBKuD3atXWIns9&gclid=Cj0KCQjws4fEBhD-ARIsACC3d29RPpP_iW5Hd3vhzPgGpjMQVnpXV8aORMdJg0VimjkpqAeafoWj9jAaAqcTEALw_wcB&gclsrc=aw.ds&vjk=ca9fc981455db544" target="_blank">INDEED</a></li>
             </ol>
         </div>
     </div>
