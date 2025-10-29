044080df94ea3737c2c4dbcdabb2d2dd420884d2
Ernesto ML <ernesto.montano@upaep.mx>
2025-10-29 00:02:07 -0600

    Modificacion carrusel en pagina inicial

--- resources/views/components/mapa_carrusel.blade.php
+++ resources/views/components/mapa_carrusel.blade.php
@@ -10,19 +10,21 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="imagenes/FotoPortadaWeb.png" class="d-block w-100" alt="..." data-bs-interval="100">
+      <a href="{{ asset('pdfs/carruselInicio/Convocatoria_FIRMA.pdf') }}" target=”_blank”>
+        <img src="{{ asset('imagenes/carruselInicio/bannerWEB_manita.png') }}" class="d-block w-100" alt="..." data-bs-interval="100">
+      </a>
     </div>
     <div class="carousel-item">
-      <img src="imagenes/FotoPortadaWeb.png" class="d-block w-100" alt="..." data-bs-interval="100">
+      <img src="{{ asset('imagenes/carruselInicio/FotoPortadaWeb.png') }}" class="d-block w-100" alt="..." data-bs-interval="100">
     </div>
     <div class="carousel-item">
-      <img src="imagenes/Hackathon.png" class="d-block w-100" alt="..." data-bs-interval="100">
+      <img src="{{ asset('imagenes/carruselInicio/Hackathon.png') }}" class="d-block w-100" alt="..." data-bs-interval="100">
     </div>
     <div class="carousel-item">
-      <img src="imagenes/FotoPortadaWeb.png" class="d-block w-100" alt="..." data-bs-interval="100">
+      <img src="{{ asset('imagenes/carruselInicio/FotoPortadaWeb.png') }}" class="d-block w-100" alt="..." data-bs-interval="100">
     </div>
     <div class="carousel-item">
-      <img src="imagenes/Admision-docente.jpg" class="d-block w-100" alt="..." data-bs-interval="100"   >
+      <img src="{{ asset('imagenes/carruselInicio/Admision-docente.jpg') }}" class="d-block w-100" alt="..." data-bs-interval="100"   >
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carruselInicio" data-bs-slide="prev">
